8f5b221011d6cca57a95aa91754f37c259147515
Igor Zgonnik <Igor Zgonnik>
2024-04-19 10:02:53 +0300

    Initial Commit

---
+++ Spintip/SpintipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpintipApp.swift
+//  Spintip
+//
+//  Created by Igor Zgonnik on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpintipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
